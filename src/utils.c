426a74dfa3fa3b1f18a9b1a9c5fb142ba968e20a
Krystian Kaczorowski <krysor901@gmail.com>
2023-03-20 09:48:03 +0000

    merge week 2: prompt fully working

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: kkaczoro <kkaczoro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 17:38:05 by yaretel-          #+#    #+#             */
-/*   Updated: 2023/03/15 11:26:15 by yaretel-         ###   ########.fr       */
+/*   Updated: 2023/03/15 13:25:58 by kkaczoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,4 @@ size_t	strclen(const char *s, char c)
 	while (s[i] && s[i] != c)
 		i++;
 	return (i);
-}
-
-//retruns the value of the environment variable env
-char	*ft_get_env_val(char *envp[], char *env)
-{
-	int		i;
-
-	i = 0;
-	while (envp[i])
-	{
-		if (!ft_strncmp(envp[i], env, ft_strlen(env)))
-			break ;
-		i++;
-	}
-	return (ft_memchr(envp[i], '=', ft_strlen(envp[i])) + 1);
 }
