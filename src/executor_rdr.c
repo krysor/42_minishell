f4c592df0fbd1892bbfd7f01a6f615c9f5a689cc
Krystian Kaczorowski <krysor901@gmail.com>
2023-05-15 08:09:58 +0000

    fixed alone redirections, redirections with builtins, here_doc crashing

--- src/executor_rdr.c
+++ src/executor_rdr.c
@@ -6,7 +6,7 @@
 /*   By: kkaczoro <kkaczoro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 16:15:35 by kkaczoro          #+#    #+#             */
-/*   Updated: 2023/05/11 15:38:04 by kkaczoro         ###   ########.fr       */
+/*   Updated: 2023/05/15 10:01:08 by kkaczoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,70 +76,15 @@ static int	here_doc(char *delimiter, int len_delimiter)
 	{
 		if (write(pipefd[WRITE], next_line, len_next_line) == -1)
 			perror(NULL);
-		free(next_line);
+		dmy_free(next_line);
 		next_line = get_next_line(0);
 		len_next_line = ft_strlen(next_line);
 	}
 	if (next_line)
-		free(next_line);
+		dmy_free(next_line);
 	close(pipefd[WRITE]);
 	return (pipefd[READ]);
 }
-
-/*
-static void	handle_rdr(t_cmd *cmd, char *type, char *file)
-{
-	int	fd;
-	int	pipefd[2];
-
-	if (ft_strncmp(type, "<<", 3) == 0)
-	{
-		if (pipe(pipefd) == -1)
-			perror(NULL);
-		else
-			cmd->fd_in = here_doc(pipefd, file);
-		return ;
-	}
-	fd = open(file, get_oflag(type), 0644);
-	if (fd == -1)
-		perror(NULL);
-	else if (*type == '>')
-	{
-		if (cmd->fd_out >= 0)
-			close(cmd->fd_out);
-		cmd->fd_out = fd;
-	}
-	else
-	{
-		if (cmd->fd_in >= 0)
-			close(cmd->fd_in);
-		cmd->fd_in = fd;
-	}
-}
-
-static int	here_doc(int pipefd[], char *delimiter)
-{
-	char	*next_line;
-	int		len_delimiter;
-	int		len_next_line;
-
-	len_delimiter = ft_strlen(delimiter);
-	next_line = get_next_line(0);
-	len_next_line = ft_strlen(next_line);
-	while (next_line && (ft_strncmp(next_line, delimiter, len_delimiter)
-			|| len_delimiter != len_next_line - 1))
-	{
-		if (write(pipefd[WRITE], next_line, len_next_line) == -1)
-			perror("write: temp");
-		free(next_line);
-		next_line = get_next_line(0);	
-		len_next_line = ft_strlen(next_line);
-	}
-	if (next_line)
-		free(next_line);
-	close(pipefd[WRITE]);
-	return (pipefd[READ]);
-}*/
 
 static int	get_oflag(char *type)
 {
